486356100757ba0c104cf8b720b7e4e8183cd14d
paul <paul.pouj@gmx.fr>
2018-01-11 18:10:45 +0100

    google avec java

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+/*var recherche = "div id="rech"";
+recherche.replace(" " , "+");
+var resultat = "https://www.google.fr/search?source=hp&ei=sIJXWu3OFYPZwQLb35eIBg&q=" + recherche;
+}
+fuction mafonction(){
+  document.location.href="https://www.google.fr/search?source=hp&ei=sIJXWu3OFYPZwQLb35eIBg&q=" + recherche;
+}
+
+
+
+
+
+
+/*
+if (but.onclick){  document.index.html(div id="cache").style.display = block;
+}
+else {document.index.html(div id="cache").style.display = none;};
+
+
+
+
+
+
+
+
+
+
+
+/*if console.timeEnd === 10
+consol.log(img)
